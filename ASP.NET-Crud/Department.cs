7dc027773ab55bebb3055ad1f085f10c18b3fd9a
Guillermo Riaño <guilledark17@gmail.com>
2023-03-13 15:00:21 -0600

    ✅ Adding Models: Employee & Department

---
+++ ASP.NET-Crud/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_Crud
+{
+    public class Department
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+    }
+}
---
+++ ASP.NET-Crud/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace ASP.NET_Crud.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public Department departmentRef { get; set; }
+
+        public int salary { get; set; }
+
+        public string contractDate { get; set; }
+    }
+}
